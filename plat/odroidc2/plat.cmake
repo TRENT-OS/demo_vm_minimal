72815d75b31eda9fd0b297c18b288bf4302707c6
Axel Heider <axel.heider@hensoldt.net>
2023-03-17 12:40:07 +0100

    [DEV] fix Odroid-C2 support

--- plat/odroidc2/plat.cmake
+++ plat/odroidc2/plat.cmake
@@ -2,4 +2,8 @@
 # Copyright (C) 2022, HENSOLDT Cyber GmbH
 #
 
-# nothing special here
+# need to increase the size here
+set(KernelRootCNodeSizeBits 16 CACHE STRING "" FORCE)
+
+# or need to reduce the size here
+#set(CapDLLoaderMaxObjects 15000 CACHE STRING "" FORCE)
