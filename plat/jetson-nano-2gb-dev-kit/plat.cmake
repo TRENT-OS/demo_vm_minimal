5aa92c14136310887424785128e544e8629d2dc5
Sebastian Eckl <sebastian.eckl@hensoldt.net>
2024-03-15 15:36:30 +0100

    [DEV] add support for Nvidia platforms

---
+++ plat/jetson-nano-2gb-dev-kit/plat.cmake
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2023, HENSOLDT Cyber GmbH
+#
+
+# need to increase the size here
+set(KernelRootCNodeSizeBits 16 CACHE STRING "" FORCE)
+
+# or need to reduce the size here
+#set(CapDLLoaderMaxObjects 15000 CACHE STRING "" FORCE)
